51f7e455eac49c6edda5724863bd60b535b7585d
Guilherme <a.guilhermesena@gmail.com>
2025-01-24 01:49:29 -0400

    Trying to implement bubble behavior

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class BubbleController : MonoBehaviour
 {
-    // Start is called before the first frame update
     private float _bubbleX = 1;
+    
     private float _bubbley = 1;
+    [SerializeField] private float _bubbleSpeed = 1.5f;
+    private bool canMove;
     void Start()
     {
-        
     }
+    void Update(){
+        ControllSize();
+        ApplyMovement();
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    }
+    private void ControllSize(){
         if(Input.GetKey(KeyCode.Space) == true ){
+            _bubbleSpeed = 1.5f;
             _bubbleX += 1 * Time.deltaTime;
             _bubbley += 1 * Time.deltaTime;
         transform.localScale = new Vector3(_bubbleX,_bubbley,1);
-        Debug.Log("CRESCENDO");
         }
         
     }
-}
+    void ApplyMovement(){
+
+            if(Input.GetKey(KeyCode.Space) == false){
+           gameObject.transform.Translate(0f ,transform.position.y * _bubbleSpeed * Time.deltaTime,0f);
+        
+        }
+
+
+        }
+    }
+    
